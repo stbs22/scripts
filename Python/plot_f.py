c4b529383e1afce4aecf48d8470755f2b2321cd3
Esteban <steveehs2000@gmail.com>
2021-04-28 01:05:31 -0400

    Python/Tarea1.ipynb Python/plot_f.py R/commands.r Python/Tarea2.ipynb Python/plot.py

--- Python/plot_f.py
+++ Python/plot_f.py
@@ -1,1 +1,19 @@
-data = pd.read_csv("datap1.csv",sep=";")
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv("/home/_init_/Documentos/Data/FCD/datap1.csv",sep=",")
+#data = pd.read_csv("/home/_init_/Documentos/Data/FCD/data.csv",sep=";")
+
+edad = data["Age"]
+
+limpio = edad[edad.notna()]
+
+print("Promedio:",edad.mean())
+print("Promedio:",limpio.mean())
+
+#plt.boxplot(limpio, vert = False)
+plt.hist(limpio,bins=40)
+    
+#plt.xlim(0,100)
+
+plt.show()
+
--- R/commands.r
+++ R/commands.r
@@ -3,10 +3,11 @@ library(readxl)
 
 setwd("/home/_init_/Documentos/Data/FCD/")
 
-base = read.csv(file.choose(),sep = ";")
+base = read.csv("/home/_init_/Documentos/Data/FCD/datap1.csv", sep = ",")
 
 summary(base)
 
+
 colnames(base)
 
-boxplot(base$height, horizontal = T, main = 'Peso', col= rainbow(1) )
+boxplot(base$Age, horizontal = T, main = 'EDAD', col= rainbow(1) )
