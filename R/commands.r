ac54f952d232862e48a3246602dbe9f3482b8c03
Esteban <steveehs2000@gmail.com>
2021-05-06 13:31:20 -0400

    Data/FCD/data_continenete.csv R/commands.r

--- R/commands.r
+++ R/commands.r
@@ -1,22 +1,39 @@
 install.packages("readxl")
 library(readxl)
 
-setwd("/home/_init_/Documentos/Scripts/GIT/scripts")
-setwd("C:/Users/ESTEBAN/Documents/Scripts_U/git/scripts/Data/FCD")
+setwd("/home/_init_/Documentos/Scripts/GIT/scripts/Data/FCD/"); base = read.csv("datap1.csv", sep = ",")
 
-base = read.csv("C:/Users/ESTEBAN/Desktop/datacountry.csv", sep = ",")
-basep = read.csv("datap1.csv", sep = ",")
-base_noc = base$Three_Letter_Country_Code
-table(base$Games)[a + 50]
+setwd("C:/Users/ESTEBAN/Documents/Scripts_U/git/scripts/Data/FCD"); base = read.csv("C:/Users/ESTEBAN/Desktop/datacountry.csv", sep = ",")
+
+colnames(base)
+
+table(base$Games)
+mean(base$Age)
+
+limpia = subset(base,!(is.na(base$Age)))
+
+typeof(table(limpia$Age))
+sort(table(limpia$Team))
+mean(subset(limpia, limpia$Team == "United States")$Age)
+mean(subset(limpia, limpia$Team == "France")$Age)
+mean(subset(limpia, limpia$Team == "Great Britain")$Age)
+mean(subset(limpia, limpia$Team == "Italy")$Age)
+
+barplot(table(limpia$Medal))
+mean(subset(limpia, limpia$Medal == "Gold")$Age)
+mean(subset(limpia, limpia$Medal == "Silver")$Age)
+mean(subset(limpia, limpia$Medal == "Bronze")$Age)
+
+length(sort(table(limpia$Sport)))
+dep = names(sort(table(limpia$Sport)))
+lista = list()
 
 
-base$Games
-table(base$Games,base$Year)
-summary(base)
+for(i in 1:66){
+  print(dep[i])
+  print(mean(subset(limpia, limpia$Sport == dep[i])$Age))
+  lista[i] = mean(subset(limpia, limpia$Sport == dep[i])$Age)
+}
 
-noc = table(base$NOC)
-nom = names(noc)
-
-write.table(nom, file ="noc.txt", sep = " ", row.names = FALSE, fileEncoding = "")
-
-boxplot(base$Age, horizontal = T, main = 'EDAD', col= rainbow(1) )
+lista[66]
+barplot(base$games, horizontal = T, main = 'EDAD', col= rainbow(1) )
