208520acee582a4c8d38542ea5253a8df377d979
Esteban <steveehs2000@gmail.com>
2021-05-07 00:22:17 -0400

    Python/plot.ipynb R/commands.r

--- R/commands.r
+++ R/commands.r
@@ -1,61 +1,69 @@
 install.packages("readxl")
+
 library(readxl)
 options(max.print = 9999999)
 
-setwd("/home/_init_/Documentos/Scripts/GIT/scripts/Data/FCD/"); base = read.csv("datap1.csv", sep = ",")
+setwd("/home/_init_/Documentos/Scripts/GIT/scripts/Data/FCD/"); base_na = read.csv("datap1.csv", sep = ","); base = subset(base,!(is.na(base_na$Age)))
 
-setwd("C:/Users/ESTEBAN/Documents/Scripts_U/git/scripts/Data/FCD"); base = read.csv("datap1.csv", sep = ",")
+setwd("C:/Users/ESTEBAN/Documents/Scripts_U/git/scripts/Data/FCD"); base_na = read.csv("datap1.csv", sep = ","); base = subset(base,!(is.na(base_na$Age)))
 
 colnames(base)
 
 mean(base$Age)
 
-limpia = subset(base,!(is.na(base$Age)))
 
-typeof(table(limpia$Age))
+typeof(table(base$Age))
 
-as.data.frame(sort(table(limpia$Team)))
-mean(subset(limpia, limpia$Team == "United States")$Age)
-mean(subset(limpia, limpia$Team == "France")$Age)
-mean(subset(limpia, limpia$Team == "Great Britain")$Age)
-mean(subset(limpia, limpia$Team == "Italy")$Age)
+as.data.frame(sort(table(base$Team)))
+mean(subset(base, base$Team == "United States")$Age)
+mean(subset(base, base$Team == "France")$Age)
+mean(subset(base, base$Team == "Great Britain")$Age)
+mean(subset(base, base$Team == "Italy")$Age)
 
-barplot(table(limpia$Medal))
-mean(subset(limpia, limpia$Medal == "Gold")$Age)
-mean(subset(limpia, limpia$Medal == "Silver")$Age)
-mean(subset(limpia, limpia$Medal == "Bronze")$Age)
-mean(subset(limpia, is.na(limpia$Medal))$Age)
+barplot(table(base$Medal))
+mean(subset(base, base$Medal == "Gold")$Age)
+mean(subset(base, base$Medal == "Silver")$Age)
+mean(subset(base, base$Medal == "Bronze")$Age)
+mean(subset(base, is.na(base$Medal))$Age)
 
-as.data.frame(sort(table(limpia$Sport)))
+table(base$Year)
 
-df = as.data.frame(),
-`colnames<-`(df,"CTM")
-df[4] = 4
-df
+min(subset(base, base$Year == "1896")$Age)
 
-dep = as.data.frame(names(sort(table(limpia$Sport))) )
-dep
+(length(names(sort(table(base$Year)))))
+for(i in 1:(length(names(sort(table(base$Year))))) ){
+  print(names(sort(table(base$Year)[i])))
+  print(min(subset(base, base$Year == names(sort(table(base$Year)[i])))$Age))
+}
+
+as.data.frame(sort(table(base$Sport)))
+
+
 
 lista = list()
 as.data.frame(lista)
 lista 
 
 typeof(sample(1:10))
-typeof( mean(subset(limpia, limpia$Sport == dep[1])$Age) )
+typeof( mean(subset(base, base$Sport == dep[1])$Age) )
 
 as.data.frame(lista, row.names = dep)
 table(sort(boxplot.stats(base$Age)$out))
 
 length(table(base$Name))
 
-limpia$Age
-barplot(table(limpia$Age), main = "Frecuencia Edades", col = rainbow(2))
+base$Age
+barplot(table(base$Age), main = "Frecuencia Edades", col = rainbow(2))
 
-mean(subset(limpia, limpia$Sport == dep)$Age)
+dep = (names(sort(table(base$Sport))) )
+dep
 
-for(i in 1:66){
-  print(mean(subset(limpia, limpia$Sport == dep)$Age))
-  lista[i] = mean(subset(limpia, limpia$Sport == dep[i])$Age)
+mean(subset(base, base$Sport == dep)$Age)
+
+(length(names(sort(table(base$Sport)))))
+for(i in 1:(length(names(sort(table(base$Sport))))) ){
+  print(names(sort(table(base$Sport)[i])))
+  print(mean(subset(base, base$Sport == names(sort(table(base$Sport)[i])))$Age))
 }
 
 lista[66]
