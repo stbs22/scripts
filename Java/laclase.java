8e3d08c5170a352d090dbc720c401da457513b8d
stbs22 <steveehs2000@gmail.com>
2021-05-11 05:00:04 -0400

    Java/ctm.java Java/first.java Java/laclase.java Java/unctm.java

---
+++ Java/laclase.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+
+public class laclase {
+
+    public static void main(String args[]) { //metodo main que recive arreglos de strings
+        
+        /*
+            1. byte
+            2. short 
+            3. int  
+            4. long 
+            5. float 
+            6. double 
+            7. char 
+            8. boolean
+        */
+
+        unctm martinqlo = new unctm();
+
+        //martinqlo.setObo("buenas tardes");
+
+        Scanner fuck = new Scanner(System.in);
+        System.out.print("Suxiona: ");
+        String a = fuck.nextLine();
+
+        String var = "Mira qlo imprimiste: ";
+        System.out.println(var + a);
+
+        System.out.println(martinqlo.getRaja());
+
+        fuck.close();
+
+        
+        //Clase System.
+        //Atributo out.
+        //Metodo println()
+        //var tipo de instancia texto
+    }
+}
