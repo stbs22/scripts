dfec063b14b878d647ffcea57dc0f6fcc5b9cfa0
stbs22 <steveehs2000@gmail.com>
2021-06-26 19:00:58 -0400

    nuevo archivo:  Python/app3/Grupo10-app3.zip 	nuevo archivo:  Python/app3/__pycache__/aux.cpython-39.pyc 	nuevo archivo:  Python/app3/__pycache__/lib.cpython-39.pyc 	nuevo archivo:  Python/app3/app3.pdf 	nuevo archivo:  Python/app3/app3/main.py 	nuevo archivo:  Python/app3/aux.py 	nuevo archivo:  Python/app3/input.txt 	nuevo archivo:  Python/app3/input_caso.txt 	nuevo archivo:  Python/app3/input_hard.txt 	nuevo archivo:  Python/app3/input_largo.txt 	nuevo archivo:  Python/app3/input_simple.txt 	nuevo archivo:  Python/app3/lib.py 	modificado:     Python/funcional/or_funcional.py 	nuevo archivo:  Python/matrices.py
    	borrado:        Python/websraper/wea.js

--- Python/funcional/or_funcional.py
+++ Python/funcional/or_funcional.py
@@ -1,6 +1,7 @@
 import math
 from functools import reduce 
 
+print("!=!=!=!=!=\n")
 
 class CountDown:
 
@@ -17,7 +18,14 @@ class CountDown:
       self.current -= 1
       return self.current+1
 
+def mifuncion_def(x,y):
+  return(x+y)
 
+mifuncion_lamda = lambda x,y: x+y
+
+print("1+2+3+4 =",reduce(mifuncion_lamda, [1, 2, 3, 4]))
+
+print("con 5,4: ",mifuncion_def(5,4)," == ",mifuncion_lamda(5,4))
 
 def count_untill(N):
   yield from range(0,N)
@@ -25,37 +33,11 @@ def count_untill(N):
 def print_list(list):
     print(i for i in list)
 
+
 var = [count_untill(10)]
 
 print(type(var))
 print(var)
 print_list(var)
 
-# print([x for x in a])
-
-# lista2 = [x for x in a if x < 10]
-# print(lista2)
-
-
-# for i in range(10,0,-1):
-#   print(i)
-
-
-# c2 = CountDown(10)
-# lista = [x for x in map(math.sqrt, c2)]
-# print(lista)
-
-# c3 = CountDown(10)
-# mifuncion = lambda x:x+1
-# lista2 = [x for x in map(mifuncion, c3)]
-# print(lista2)
-
-# c4 = CountDown(10)
-# mifuncion2 = lambda x,y:x if x < y else y
-# resultado = reduce(mifuncion2, c4)
-# print(resultado)
-
-# c5 = CountDown(10)
-# mifuncion = lambda x:x>5
-# lista2 = [x for x in filter(mifuncion, c3)]
-# print(lista2)
+print("\n!=!=!=!=!=")
