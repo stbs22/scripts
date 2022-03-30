ca27d442ac9422205e8dfee13dd09cb5e44a5da2
stbs22 <steveehs2000@gmail.com>
2022-03-30 12:29:46 -0300

    Update C

---
+++ C/fswap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void swap(int* a, int* b){
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+int main(void){
+	
+	int a = 9;
+	int b = 4;
+	
+	printf("\na: %i[%p] - b: %i[%p]\n",a,&a,b,&b);
+	
+	/* Siendo función, espacios de memoria no cambián */	
+	int temp;
+	temp = a;
+	a =  b;
+	b = temp;
+
+	printf("\nSin Función:\na: %i[%p] - b: %i[%p]\n",a,&a,b,&b);
+	swap(&a,&b);
+	printf("\nFunción swap:\na: %i[%p] - b: %i[%p]\n",a,&a,b,&b);
+	
+	
+	return 0;
+} 
