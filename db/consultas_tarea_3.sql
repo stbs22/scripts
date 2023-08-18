be216e5be7c223f3b1299541a8c8e8e517197dda
stbs22 <steveehs2000@gmail.com>
2023-08-18 11:22:29 -0400

    vie 18 ago 2023 11:22:28 -04

--- db/consultas_tarea_3.sql
+++ db/consultas_tarea_3.sql
@@ -3,89 +3,136 @@
  * 19-06-2023
  * Autor: Esteban Hernández
  * 
- * Se ha utilizado Dbeaver 23.1.0 como cliente para base de datos
+ * Se ha utilizado Dbeaver 23.1.0 como cliente para acceder a la base de datos
  * 
+
+TABLES: titles; names; categories; directed; acted
 */
 
+--
+-- PARTE 1
+--
 
 --Respuesta 1
-select * from titles
-join categories on titles.id = title_id
-where category = 'Mystery' and release_year >= 2020;
+with main_aux(actor,id_pelicula) as 
+(	
+	/* Tabla con actores e ID de película */
+ 	select distinct "name", acted.title_id from names
+	join acted on acted.name_id = names.id -- where acted.title_id=1
+)
+SELECT aux_1.actor AS actor1, --aux_1.id_pelicula as id_pelicula
+aux_2.actor AS actor2
+from main_aux as aux_1
+join main_aux as aux_2
+on aux_1.id_pelicula = aux_2.id_pelicula
+where aux_1.actor <> aux_2.actor; -- and aux_1.id_pelicula = 1;
 
 
 --Respuesta 2
-select title, rating from titles
-order by rating desc limit 10;
+with recursive co_stars(actor1, actor2) as 
+(
+	with main_aux(actor,id_pelicula) as 
+	(
+		select distinct "name", acted.title_id from names
+		join acted on acted.name_id = names.id
+	) 
+	select aux_1.actor AS actor1, aux_2.actor AS actor2 
+	from main_aux as aux_1
+	join main_aux as aux_2 on aux_1.id_pelicula = aux_2.id_pelicula
+	where aux_1.actor <> aux_2.actor
+), 
+indirect_co_star_bacon(actor) as (
+	select co_stars.actor2 as actor from co_stars
+	where co_stars.actor1 = 'Kevin Bacon'
+    union
+    select aux.actor2 as actor from indirect_co_star_bacon as ind_aux
+    join co_stars as aux on ind_aux.actor = aux.actor1
+)
+select distinct indirect_co_star_bacon.actor from indirect_co_star_bacon; 
 
 
 --Respuesta 3
-select "name", death_year, category, role from names
-join acted on id = name_id
-join categories on acted.title_id = categories.title_id
-where death_year isnull and category = 'Horror';
-
+with recursive co_stars(actor1, actor2) as 
+(
+	with main_aux(actor,id_pelicula) as 
+	(
+		select distinct "name", acted.title_id from names
+		join acted on acted.name_id = names.id
+	) 
+	select aux_1.actor AS actor1, aux_2.actor AS actor2 
+	from main_aux as aux_1
+	join main_aux as aux_2 on aux_1.id_pelicula = aux_2.id_pelicula
+	where aux_1.actor <> aux_2.actor
+),indirect_co_star_carruth(actor) as (
+	select co_stars.actor2 as actor from co_stars
+	where co_stars.actor1 = 'Shane Carruth'
+    union
+    select aux.actor2 as actor from indirect_co_star_carruth as ind_aux
+    join co_stars as aux on ind_aux.actor = aux.actor1
+)
+select distinct indirect_co_star_carruth.actor from indirect_co_star_carruth; 
 
 --Respuesta 4
-select distinct "name", birth_year, death_year,role  from names
-join acted on id = name_id
-join titles on acted.title_id = titles.id
-where acted.role = 'Batman';
-
-
---Respuesta 5
-select distinct "name" from names
-where names.id in 
+with recursive co_stars(actor1, actor2) as 
 (
-	select name_id from acted
-	where acted.name_id in 
-		(select name_id from directed)
-);
-
-/*
---Consulta para corroborar respuesta anterior--
-
-select names.name, titles.title from acted
-join titles on acted.title_id = titles.id
-join names on name_id = names.id 
-where acted.name_id in (select name_id from directed);
-*/
-
-
---Respuesta 6
-select titles.title from titles
-join acted on titles.id = acted.title_id  
-join names on names.id = acted.name_id
-where names."name" = 'Joaquin Phoenix';
+	with main_aux(actor,id_pelicula) as 
+	(
+		select distinct "name", acted.title_id from names
+		join acted on acted.name_id = names.id
+	) 
+	select aux_1.actor AS actor1, aux_2.actor AS actor2 
+	from main_aux as aux_1
+	join main_aux as aux_2 on aux_1.id_pelicula = aux_2.id_pelicula
+	where aux_1.actor <> aux_2.actor
+), 
+indirect_co_star_bacon(actor1,actor2) as (
+	select co_stars.actor1 as actor1, co_stars.actor2 as actor2 from co_stars
+	where co_stars.actor1 = 'Kevin Bacon'
+    union
+    select ind_aux.actor2 as actor1, aux.actor2 as actor2 from indirect_co_star_bacon as ind_aux
+    join co_stars as aux on ind_aux.actor2 = aux.actor1
+)
+select distinct * from indirect_co_star_bacon; 
 
 
---Respuesta 7
-select category, count(title_id) as number_of_titles  from categories
-group by category;
+--
+-- PARTE 2
+--
 
 
---Respuesta 8
-select titles.title, count(name_id) as number_of_actors 
-from (select distinct name_id, title_id from acted) as tabla
-join titles on id = tabla.title_id
-group by titles.title having count(tabla.name_id) >= 10
-order by number_of_actors desc;
+--Respuesta 1
+
+/* CON JOIN */
+explain analyze 
+select distinct "name" from names
+join acted on names.id = acted.name_id
+where acted."role" = 'Batman'
+order by "name" asc;
+
+/* SIN JOIN */
+explain analyze 
+select distinct "name" from names, acted
+where names.id = acted.name_id  and "role" = 'Batman';
 
 
---Respuesta 9
-select names."name", count(tabla.title_id) as number_of_titles 
-from (select distinct name_id, title_id from acted) as tabla
-join names on id = tabla.name_id 
-group by names."name" order by number_of_titles desc;
+--Respuesta 2
+explain analyze 
+select distinct "name" from names
+join acted on names.id = acted.name_id
+where acted."role" = 'Batman';
+
+--Respuesta 3
+explain analyze
+select * from titles
+where titles.release_year = 2022;
+
+--Respuesta 4
+explain analyze
+select * from titles
+where titles.release_year <= 2018 and titles.release_year >= 2020;
 
 
-
---Respuesta 10
-select tabla.category, tabla.max_rating, titles.title from (
-	select category, max(titles.rating) as max_rating from categories
-	join titles on id = categories.title_id 
-	group by category
-) as tabla
-join titles on tabla.max_rating = titles.rating
-order by category;
-
+SELECT tablename, indexname, indexdef
+FROM pg_indexes
+WHERE schemaname = 'public'
+ORDER BY tablename, indexname;
